9514c7277e7cdcef723b298fdee2971fe173f49d
takshvi123 <targetolympiad@gmail.com>
2025-02-20 15:20:35 +0530

    commit

--- Armstrong_number.py
+++ Armstrong_number.py
@@ -1,11 +1,15 @@
-num=int(input("Enter the number you think is a amstrong number "))
-sum=0
-temp=num
-while temp>0:
-    digit=temp%10
-    sum=sum+(digit**3)
-    temp=temp//10
-if num==sum:
-    print(f"{num} is a amstrong number")
+num = int(input("Enter the number you think is an Armstrong number: "))
+sum = 0
+temp = num
+n = len(str(num))  # Number of digits in the number
+
+while temp > 0:
+    digit = temp % 10
+    sum = sum + (digit ** n)
+    temp = temp // 10
+
+if num == sum:
+    print(f"{num} is an Armstrong number")
 else:
-    print(f"{num} is not a amstrong number")
+    print(f"{num} is not an Armstrong number")
+
